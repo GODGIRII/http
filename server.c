414bdf2352293ef019b98ba14626ad073d0c9874
GODGIRII <rishabgirii28@gmail.com>
2026-04-06 19:15:39 +0530

    addec server.c & removed main.cpp

---
+++ server.c
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "server.h"
+#include <sys/types.h>
+#include <sys/types.h>
+
