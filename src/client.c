ebadd26e7d680a4a0c0123b6606ef5e2349c9d6d
GODGIRII <rishabgirii28@gmail.com>
2026-04-10 13:09:03 +0530

    created include folder

--- src/client.c
+++ src/client.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 
 int int main(int argc, char *argv[])
 {
